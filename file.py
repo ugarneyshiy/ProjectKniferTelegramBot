75902c6095f77754ae700b52c457dc80635c7991
ugarneyshiy <ugarneyshiy@gmail.com>
2024-07-02 18:48:06 +0400

    long pooling logic

--- file.py
+++ file.py
@@ -6,6 +6,7 @@ API_URL = 'https://api.telegram.org/bot'
 BOT_TOKEN = '7426418619:AAG8xOGqSJKqGoaicg8s0R0_TrdgUCsYkAA'
 
 offset = -2
+timeout = 60
 updates: dict
 
 
@@ -15,13 +16,12 @@ def do_something() -> None:
 
 while True:
     start_time = time.time()
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             do_something()
 
-    time.sleep(3)
     end_time = time.time()
     print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
