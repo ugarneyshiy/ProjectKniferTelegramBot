03710b3ae45fe1137168a9b8b631d47c9e4cd463
ugarneyshiy <ugarneyshiy@gmail.com>
2024-07-02 18:46:24 +0400

    pooling logic

--- file.py
+++ file.py
@@ -1,34 +1,27 @@
 import requests
 import time
 
+
 API_URL = 'https://api.telegram.org/bot'
 BOT_TOKEN = '7426418619:AAG8xOGqSJKqGoaicg8s0R0_TrdgUCsYkAA'
-API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
-ERROR_TEXT = 'Здесь должна была быть картинка с котиком :('
-TEXT = "Ура! Классный апдейт!"
-MAX_COUNTER = 100
 
 offset = -2
-counter = 0
-cat_response: requests.Response
-cat_link: str
+updates: dict
 
 
-while counter < 100:
-    print('attempt =', counter)
+def do_something() -> None:
+    print('Был апдейт')
+
+
+while True:
+    start_time = time.time()
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            cat_response = requests.get(API_CATS_URL)
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()[0]['url']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+            do_something()
 
-    time.sleep(1)
-    counter += 1
-
+    time.sleep(3)
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
