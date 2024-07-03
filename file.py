454c8daa96e3cc6fdea455c9cafe07063ea74408
ugarneyshiy <ugarneyshiy@gmail.com>
2024-07-03 19:00:10 +0400

    echo bot with different input types

--- file.py
+++ file.py
@@ -12,11 +12,13 @@ dp = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands='start'))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
@@ -24,16 +26,18 @@ async def process_help_command(message: Message):
     )
 
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
 # кроме команд "/start" и "/help"
+@dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+                 'методом send_copy'
+        )
 
-
-# Регистрируем хэндлеры
-dp.message.register(process_start_command, Command(commands='start'))
-dp.message.register(process_help_command, Command(commands='help'))
-dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
